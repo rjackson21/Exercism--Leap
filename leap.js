7fb828123f863420e38cb275556d1011ac3cd8dc
Regina Jackson <rjackson156@gmail.com>
2022-03-22 19:03:51 -0400

    first commit

---
+++ leap.js
@@ -0,0 +1,22 @@
+//
+// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const isLeap = (year) => {
+  if (year % 400 == 0 && year % 100 == 0 && year % 4 == 0){
+    return true;
+  }
+  else if (year % 4 == 0 && year % 100 == 0){
+    return false;
+  }
+  else if (year % 4 == 0){
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+let year = 2000
+console.log(isLeap(year))
